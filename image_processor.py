8a4cf4497d0c33d63c1361bb10543ab86f793948
Aronish <aron.samulin@gmail.com>
2024-02-11 23:11:25 +0200

    Support any aspect ratio

--- image_processor.py
+++ image_processor.py
@@ -3,12 +3,18 @@ from PIL import ImageTk, Image as PImage
 import tkinter
 
 class Viewer:
-    def __init__(self, window_size, image_path) -> None:
-        self.original_image = PImage.open(image_path).resize((int(window_size[0] / 2), window_size[1]))
+    def __init__(self, requested_window_height: int, file_path) -> None:
+        self.original_image = PImage.open(file_path)
+
+        aspect_ratio = self.original_image.width / self.original_image.height
+        self.height = requested_window_height
+        self.width = int(aspect_ratio * self.height)
+        self.original_image = self.original_image.resize((self.width, self.height))
+
         self.org_photo_img = ImageTk.PhotoImage(self.original_image)
 
     def getModifiedPhotoImg(self, kernel_type):
-        # Updates the PhotoImage with a kernel
+        # Use C++ class to get a modified image back
         self.convertible_image = ConvertibleImage(self.original_image.width, self.original_image.height, list(self.original_image.tobytes()))
         self.convertible_photo_img = ImageTk.PhotoImage(image=PImage.frombytes(data=bytes(self.convertible_image.applyKernel(kernel_type)), mode=self.original_image.mode, size=self.original_image.size))
         return self.convertible_photo_img
@@ -23,17 +29,16 @@ def main():
             break
         except FileNotFoundError:
             print("File does not exist!")
-
     # Create a simple window
-    width = 1200
-    height = 600
     root = tkinter.Tk()
     root.title("Image Processor")
     root.resizable(False, False)
     root.lift()
     root.attributes("-topmost", True)
     root.attributes("-topmost", False)
-    canvas = tkinter.Canvas(root, width=width, height=height)
+
+    viewer = Viewer(400, file_path)
+    canvas = tkinter.Canvas(root, width=viewer.width * 2, height=viewer.height) # Canvas with space for two images side-by-side
 
     kernel_choice = tkinter.IntVar()
     def switch_kernel():
@@ -45,11 +50,10 @@ def main():
         radio_button = tkinter.Radiobutton(root, text=list(KernelType.__members__.keys())[i], variable=kernel_choice, value=i, command=switch_kernel).grid(row=0, column=i)
         radio_buttons.append(radio_button)
 
-    viewer = Viewer((width, height), file_path)
     # Display original image
     canvas.create_image(0, 0, anchor=tkinter.NW, image=viewer.org_photo_img)
     # Display modified image
-    convertible_canvas_img = canvas.create_image(int(width / 2), 0, anchor=tkinter.NW, image=viewer.getModifiedPhotoImg(KernelType.NOTHING))
+    convertible_canvas_img = canvas.create_image(viewer.width, 0, anchor=tkinter.NW, image=viewer.getModifiedPhotoImg(KernelType.NOTHING))
     canvas.grid(row=1, column=0, columnspan=len(radio_buttons))
 
     root.mainloop()
