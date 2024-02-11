42438553127f587ffc1e87b54cad8b4eb5e8c03a
Aronish <aron.samulin@gmail.com>
2024-02-11 21:03:16 +0200

    Fix project name

--- build/cmake_install.cmake
+++ build/cmake_install.cmake
@@ -1,8 +1,8 @@
-# Install script for directory: C:/dev/vtt/gaussian_blur
+# Install script for directory: C:/dev/vtt/image_processor
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
-  set(CMAKE_INSTALL_PREFIX "C:/Program Files/convertible_image")
+  set(CMAKE_INSTALL_PREFIX "C:/Program Files/image_processor")
 endif()
 string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
 
@@ -34,7 +34,7 @@ endif()
 
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   # Include the install script for the subdirectory.
-  include("C:/dev/vtt/gaussian_blur/build/lib/pybind11/cmake_install.cmake")
+  include("C:/dev/vtt/image_processor/build/lib/pybind11/cmake_install.cmake")
 endif()
 
 if(CMAKE_INSTALL_COMPONENT)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/dev/vtt/gaussian_blur/build/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "C:/dev/vtt/image_processor/build/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
